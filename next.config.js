050694097063f5f28526d09b8ae08724e4d3a4fa
huayemao <huayemao4g@gmail.com>
2022-10-15 03:00:10 +0800

    feat: 离线优先

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-  reactStrictMode: true,
   pwa: {
-    dest: 'public',
+    dest: "public",
+    dynamicStartUrl: false,
+    runtimeCaching: [
+      {
+        urlPattern: /.*/i,
+        handler: "CacheOnly",
+        options: {
+          cacheName: "whole-site",
+          expiration: {
+            maxEntries: 200,
+            maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+          },
+        },
+      },
+    ],
   },
-})
+});
